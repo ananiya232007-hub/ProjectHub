aa0b4679ba4b0981a75d8d6717c8020ed90d0f68
Ananiya <ananiya232007@gmail.com>
2026-06-14 01:28:41 +0530

    fix render routing

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static((__dirname)));
 /* ---------------- ROOT ROUTE ---------------- */
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "login.html"));
